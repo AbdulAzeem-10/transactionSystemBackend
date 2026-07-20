f1d0acb02cbd4ccd69c679e14cc15d01ac168679
DevAzeem <abdulazeemjd@gmail.com>
2026-07-20 17:23:42 +0500

    login routes and controller handled

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,8 +3,8 @@ const jwt=require("jsonwebtoken");
 
 
 /**
-* user register controller 
-* POST /api/auth/register
+*- user register controller 
+*-POST /api/auth/register
 */
 async function userRegisterController(req,res){
 //controller will get some data we use to create a user
@@ -50,6 +50,52 @@ let {email,password,name}=req.body;
 }
 //register api complete
 
+
+/**
+ *- user login controller
+ *- POST api/auth/login
+ */
+async function userLoginController(req,res){
+    const {email,password,name}=req.body;
+    const user=await userModel.findOne({email}).select("+password");//->this is why there was error as password is not being selected from usermodel.js use select here for password
+
+    if(!user){
+        return res.status(401).json({
+            message:"invalid password or email"
+        })
+    }
+
+    const isValidPass=await user.comparePassword(password);
+
+    if(!isValidPass){
+        return res.status(401).json({
+            message:"password or email invalid"
+        })
+    }
+
+     //need private key for jwt
+    const token=jwt.sign({userId:user._id}
+        ,process.env.JWT_SECRET,
+        {expiresIn:"3d"}
+    );//->send payload here
+
+    //saving token
+    res.cookie("token",token);
+
+    //whenever we make new endpoint or new resource in api because of users request hence we send 201 status
+    res.status(200).json({
+        user:{
+            _id:user._id,
+            email:user.email,
+            name:user.name
+        },
+        token
+    })
+}
+
+
+
 module.exports={
-    userRegisterController
+    userRegisterController,
+    userLoginController
 }
